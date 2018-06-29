19cd144ad59c4ab3bf50ca53ba6e7c14149413c8
lukakern <lukern@gmx.de>
2018-06-29 19:18:50 +0200

    third commit

--- Project_06_Rasterizer_B.py
+++ Project_06_Rasterizer_B.py
@@ -10,3 +10,20 @@ Salz & Pfeffer
 
 Spargel in nassem Küchenhandtuch im Kühlschrank aufbewahren
 '''
+
+
+from shapely.geometry import Point, Polygon, MultiPoint, collection
+import numpy as np
+
+
+attributes = (5, 19, 7)
+np.random.seed(None)
+geometry_list = collection.GeometryCollection(
+    [MultiPoint(
+        [Point(*_) for _ in np.random.gamma(mid, 3, (15, 2))]
+    ).convex_hull for mid in (10, 15, 40)]
+)
+geometry_list
+
+
+list(zip(geometry_list, attributes))
