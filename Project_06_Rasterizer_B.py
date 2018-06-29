76c523a2bc75fe28851592825eb97823f02efb05
Luka Kern <lukakern@ub-haus-10-126-76-21.eduroam-ub-haus.privat>
2018-06-29 17:28:37 +0200

    first commit

---
+++ Project_06_Rasterizer_B.py
@@ -0,0 +1,12 @@
+'''
+Spargel Rezept:
+Spargel in Bratolive angebraten
+wenn anfang braun Butter dazu + Muskat + Zucker
+halber brühwurfel mit wasser
+weißwein essig
+So viel Butter wie sonst geschmolzen (1/8 - 1/4)
+Wasser dazu so wie es passt zur flüssigkeit
+Salz & Pfeffer
+
+Spargel in nassem Küchenhandtuch im Kühlschrank aufbewahren
+'''
